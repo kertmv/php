c63d7258fc4142c1586c38bbe4e769c106313c12
kertmv <115462783+kertmv@users.noreply.github.com>
2024-06-03 19:19:20 +0300

    Update ul9.php

--- ul9.php
+++ ul9.php
@@ -3,12 +3,12 @@
 <html>
 <head>
 <meta charset="utf-8">
-<title>09 - PHP - Tekstifunktsioonid</title>
+<title>PHP 09</title>
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 </head>
 <body>
 <div class="container">
-<h2>Sisesta oma nimi</h2>
+<p>Sisesta oma nimi</p>
 <form action="" method="get">
     <label for="nimi">Nimi:</label>
     <input type="text" id="nimi" name="nimi">
@@ -24,7 +24,7 @@ if (isset($_GET["nimi"])) {
     echo "<p>Palun sisesta oma nimi!</p>";
 }
 ?>
-<h2>Sisesta oma sonad</h2>
+<p>Sisesta oma sonad</p>
 <form action="" method="get">
     <label for="sonad">sonad:</label>
     <input type="text" id="sonad" name="sonad">
@@ -39,7 +39,7 @@ if (isset($_GET["nimi"])) {
 
 
 <form method="get" action="">
-<h2>Ropp sona tarnideks ropud sonad on noob, loll ja idioot </h2>
+<p>Ropp sona tarnideks ropud sonad on noob, loll ja idioot </p>
     <label for="lause">Sisesta lause:</label><br>
     <textarea id="lause" name="lause" rows="4" cols="50"></textarea><br>
     <input type="submit" value="Korrasta">
@@ -58,7 +58,7 @@ if (isset($_GET["nimi"])) {
         echo "<p>Korras lause: $korraslause</p>";
     ?>
 <form method="get" action="">
-    <h2>Gmaili tegemine</h2>
+    <p>Gmaili tegemine</p>
     <label for="enimi">Eesnimi:</label><br>
     <input type="text" id="enimi" name="enimi"><br>
     <label for="pnimi">Perenimi:</label><br>
@@ -82,6 +82,6 @@ if (isset($_GET["nimi"])) {
 </html>
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.3/dist/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWpIMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 </body>
 </html>
