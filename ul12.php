2dcd701071c817f927185710bcccffbbb5dbf1cc
kertmv <115462783+kertmv@users.noreply.github.com>
2024-06-03 19:19:50 +0300

    Update ul12.php

--- ul12.php
+++ ul12.php
@@ -3,12 +3,12 @@
 <html>
 <head>
 <meta charset="utf-8">
-<title>12 - PHP - too CSV failidega</title>
+<title>PHP 12</title>
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 </head>
 <body>
 <div class="container">
-<h1>Auto sõiduaeg</h1>
+<p>Auto sõiduaeg</p>
     <form method="get">
         <label for="start">Stardi aeg (hh:mm):</label>
         <input type="text" id="start" name="start" maxlength="5" required pattern="\d{2}:\d{2}">
@@ -70,6 +70,6 @@ echo "Meeste suurim palk on: $mmax<br>Naiste suurim palk on<br>";
 
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.3/dist/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWpIMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 </body>
 </html>
