e9680b064c3371d16c8de5ecfad66f0e151d8e57
kertmv <115462783+kertmv@users.noreply.github.com>
2024-06-03 19:06:31 +0300

    Update ul6.php

--- ul6.php
+++ ul6.php
@@ -2,13 +2,13 @@
 <html>
 <head>
 <meta charset="utf-8">
-<title>06 - PHP - tsüklid</title>
+<title>PHP 6</title>
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 </head>
 <body>
 <div class="container">
 <?php
-echo "<h3>Numbrid</h3>";
+echo "<p>Numbrid</p>";
 for ($i = 1; $i <= 100; $i++) {
     echo "$i. ";
     if ($i % 10 === 0) {
@@ -17,7 +17,7 @@ for ($i = 1; $i <= 100; $i++) {
 }
 ?>
 <?php
-echo "<h3>Tärnid1</h3>";
+echo "<p>Tärnid1</p>";
 $tärn = 10;
 
 for ($i = 0; $i < $tärn; $i++) {
@@ -25,7 +25,7 @@ for ($i = 0; $i < $tärn; $i++) {
 }
 ?>
 <?php
-echo "<h3>Tärnid2</h3>";
+echo "<p>Tärnid2</p>";
 $tarn2 = 5;
 
 for ($i = 0; $i < $tarn2; $i++) {
@@ -33,7 +33,7 @@ for ($i = 0; $i < $tarn2; $i++) {
 }
 ?>
 <form action="" method="get">
-<h3>Ruut</h3>
+<p>Ruut</p>
         <label for="suurus">Sisesta ruudu suurus:</label>
         <input type="number" id="suurus" name="suurus" min="1" required>
         <button type="submit">tee ruut</button>
@@ -54,7 +54,7 @@ if (isset($_GET["suurus"])) {
 }
 ?>
 <?php
-echo "<h3>paarisarv</h3>";
+echo "<p>paarisarv</p>";
 for ($i = 10; $i >= 1; $i--) {
     if ($i % 2 == 0) {
         echo $i . "<br>";
@@ -62,7 +62,7 @@ for ($i = 10; $i >= 1; $i--) {
 }
 ?>
 <?php
-echo "<h3>aint 3 jagavad arvud</h3>";
+echo "<p>aint 3 jagavad arvud</p>";
 for ($i = 1; $i <= 100; $i++) {
     if ($i % 3 == 0) {
         echo $i . "<br>";
@@ -70,7 +70,7 @@ for ($i = 1; $i <= 100; $i++) {
 }
 ?>
 <?php
-echo "<h3>Tydruk ja poiss</h3>";
+echo "<p>Tydruk ja poiss</p>";
 $poisid = array("Jaan", "Peeter", "Marko", "Andres", "Tõnu");
 $tudrukud = array("Kati", "Mari", "Liis", "Anna", "Eva");
 
@@ -84,8 +84,6 @@ if (count($poisid) == count($tudrukud)) {
 ?>
 
 
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.3/dist/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+
 </body>
 </html>
