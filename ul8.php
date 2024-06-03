32d823d2d865ca8a09c29d42d18853985b8621eb
kertmv <115462783+kertmv@users.noreply.github.com>
2024-06-03 19:08:24 +0300

    Update ul8.php

--- ul8.php
+++ ul8.php
@@ -2,18 +2,18 @@
 <html>
 <head>
 <meta charset="utf-8">
-<title>08 - PHP - Ajafunktsioonid</title>
+<title>PHP 8</title>
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 </head>
 <body>
 <div class="container">
 <?php
-echo "<h3>aeg ja kuupaev</h3>";
+echo "<p>aeg ja kuupaev</p>";
 date_default_timezone_set("Europe/Tallinn");
 $aeg = date("d.m.Y H:i");
 echo $aeg;
 ?>
-<h2>Sisesta oma sünniaasta, -kuu ja -kuupäev:</h2>
+<p>Sisesta oma sünniaasta, -kuu ja -kuupäev:</p>
     <form method="GET">
         <input type="number" name="sunniaasta" placeholder="Aasta" required min="1900" max="<?php echo date("Y"); ?>"><br><br>
         <select name="sunnikuu" required>
@@ -37,7 +37,7 @@ if (!empty($_GET["sunniaasta"]) && !empty($_GET["sunnikuu"]) && !empty($_GET["su
     $synnipaev = strtotime("$sunniaasta-$sunnikuu-$sunnikuupaev");
 
     if ($synnipaev > $aeg) {
-        echo "<h2>Sünnikuupäev ei saa olla tulevikus!</h2>";
+        echo "<p>Sünnikuupäev ei saa olla tulevikus!</p>";
         exit();
 }
 
@@ -58,10 +58,10 @@ $koolilopp = strtotime("24 June 2024");
 $aeg = time();
 $paevalles = ceil(($koolilopp - $aeg) / (60 * 60 * 24));
 
-echo "<h2>Mitu Päeva Kooli aasta lõpuni</h2>";
+echo "<p>Mitu Päeva Kooli aasta lõpuni</p>";
 echo "2022 kooliaasta lõpuni on jäänud $paevalles päeva!";
 ?>
-<h2>Vali aastaaja järgi pilt</h2>
+<p>Vali aastaaja järgi pilt</p>
     <form action="" method="get">
         <label for="aastaaeg">Vali aastaaja järgi:</label>
         <select name="aastaaeg" id="aastaaeg">
@@ -100,10 +100,5 @@ if ($imageSource) {
 }
     ?>
 
-</body>
-</html>
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.3/dist/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 </body>
 </html>
