bdd11b767017b50715cd333a510b3238f93783d3
kertmv <115462783+kertmv@users.noreply.github.com>
2024-06-03 19:02:35 +0300

    Update ul3.php

--- ul3.php
+++ ul3.php
@@ -2,13 +2,13 @@
 <html>
 <head>
 <meta charset="utf-8">
-<title>03 - PHP - Vormid info tootlemine</title>
+<title>PHP 03</title>
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 </head>
 <body>
     <div class="container">
         <h1>Harjutus 03</h1>
-        <h2>trapetsi pindala</h2>
+        <p>trapetsi pindala</p>
         <form action="#" method="get">
             <label for="a">Külg a</label>
             <input type="number" name="a" id="a"><br>
@@ -28,7 +28,7 @@
         }
         ?>
         <hr>
-        <h2>rombi ümbermoot</h2>
+        <p>rombi umbermoot</p>
         <form action="#" method="get">
             <label form="c">Rombi külg:</label>
             <input type="number" name="c" id="a">
@@ -41,9 +41,5 @@
             echo "Rombi umbermoot on ".$p."<br>";
         }
         ?>
-
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.3/dist/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 </body>
 </html>
